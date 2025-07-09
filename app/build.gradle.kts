d2fdc8e1535f6134373fb844b2e6ca120fba71d8
Lisa Schulze-Bergkamen <lisasbnza@gmail.com>
2025-07-09 09:13:30 +0200

    add room dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,4 +65,5 @@ dependencies {
     implementation(libs.hilt.android)
     ksp(libs.hilt.android.compiler)
     implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.androidx.room)
 }
