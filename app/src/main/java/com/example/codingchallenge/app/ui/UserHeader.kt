29c5eb0daa9e3e93ae258c236e8bc105c83a9164
Lisa Schulze-Bergkamen <lisasbnza@gmail.com>
2025-07-16 15:25:44 +0200

    add functionality to open system file picker ui

--- app/src/main/java/com/example/codingchallenge/app/ui/UserHeader.kt
+++ app/src/main/java/com/example/codingchallenge/app/ui/UserHeader.kt
@@ -1,6 +1,9 @@
 package com.example.codingchallenge.app.ui
 
+import android.net.Uri
 import android.util.Log
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -20,6 +23,8 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -91,6 +96,7 @@ fun UserHeader(
                 ) {
                     Text("Load Data")
                 }
+                PickDocument()
             }
 
 
@@ -119,6 +125,25 @@ fun UserHeader(
                     )
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun PickDocument() {
+    val result = remember { mutableStateOf<Uri?>(null) }
+    val launcher = rememberLauncherForActivityResult(ActivityResultContracts.OpenDocument()) {
+        result.value = it
+    }
+
+    Column {
+        Button(onClick = {
+            launcher.launch(arrayOf("application/hl7"))
+        }) {
+            Text(text = "Select Document")
+        }
+        result.value?.let { image ->
+            Text(text = "Document Path: " + image.path.toString())
         }
     }
 }
